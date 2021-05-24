f038c69a59cca6063715757dd10f20e1b62ac563
siva raghavendhar <dimplesiva@gmail.com>
2021-05-24 10:36:26 +0530

    ok smirag

---
+++ app/src/main/java/com/smirag/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.example.smirag
+
+import android.os.Bundle
+import android.text.SpannableString
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
